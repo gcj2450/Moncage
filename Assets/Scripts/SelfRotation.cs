438070aeb2bd999ae9128d5b1196421ae7b52627
DESKTOP-VES6UHG\dongz <dz1221@nyu.edu>
2018-09-29 13:59:48 -0400

    Scene Change

--- Assets/Scripts/SelfRotation.cs
+++ Assets/Scripts/SelfRotation.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SelfRotation : MonoBehaviour {
-
+	
+	public Vector3 rotationSpeed = new Vector3(0,1,0);
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +12,6 @@ public class SelfRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(0, 1, 0));
+        transform.Rotate(rotationSpeed);
 	}
 }
